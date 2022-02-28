1c9856709c8783b624e2a3acbaba38ee9287c19c
Cuneyt Akcay <cuneytakcay@gmail.com>
2022-02-27 22:20:57 -0700

    Added and styled form validation errors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,74 @@
-// const root: HTMLElement | null = document.getElementById('root')
 var root = document.getElementById('root');
+// Switch button (Dark/Light Mode)
 var switchBtn = root.querySelector('.switch');
 switchBtn.addEventListener('click', function () {
     root.classList.toggle('dark');
+});
+// Form handling
+var form = root.querySelector('form');
+var firstName = form.querySelector('[name="firstName"]');
+var lastName = form.querySelector('[name="lastName"]');
+var email = form.querySelector('[name="email"]');
+var password = form.querySelector('[name="password"]');
+var confirmPassword = form.querySelector('[name="confirmPassword"]');
+var submit = form.querySelector('[type="submit"]');
+var errorCont;
+submit.addEventListener('click', function (e) {
+    var _a, _b, _c, _d, _e, _f, _g, _h, _j, _k;
+    e.preventDefault();
+    if (firstName.value.trim() === '') {
+        errorCont = (_a = firstName
+            .closest('.input')) === null || _a === void 0 ? void 0 : _a.querySelector('.error');
+        errorCont.innerHTML =
+            '<div class="error-inner"><p><span>&#x2716;</span> Please enter your first name</p></div>';
+    }
+    else {
+        errorCont = (_b = firstName
+            .closest('.input')) === null || _b === void 0 ? void 0 : _b.querySelector('.error');
+        errorCont.innerHTML = '';
+    }
+    if (lastName.value.trim() === '') {
+        errorCont = (_c = lastName
+            .closest('.input')) === null || _c === void 0 ? void 0 : _c.querySelector('.error');
+        errorCont.innerHTML =
+            '<div class="error-inner"><p><span>&#x2716;</span> Please enter your last name</p></div>';
+    }
+    else {
+        errorCont = (_d = lastName
+            .closest('.input')) === null || _d === void 0 ? void 0 : _d.querySelector('.error');
+        errorCont.innerHTML = '';
+    }
+    if (!email.value.match(/\S+@\S+\.\S+/)) {
+        errorCont = (_e = email
+            .closest('.input')) === null || _e === void 0 ? void 0 : _e.querySelector('.error');
+        errorCont.innerHTML =
+            '<div class="error-inner"><p><span>&#x2716;</span> Enter a valid email address</p></div>';
+    }
+    else {
+        errorCont = (_f = email
+            .closest('.input')) === null || _f === void 0 ? void 0 : _f.querySelector('.error');
+        errorCont.innerHTML = '';
+    }
+    if (!password.value.match(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,}$/)) {
+        errorCont = (_g = password
+            .closest('.input')) === null || _g === void 0 ? void 0 : _g.querySelector('.error');
+        errorCont.innerHTML =
+            '<div class="error-inner"><ul><li><span>&#x2716;</span> Must be at least 6 characters long</li><li><span>&#x2716;</span> Must have at least 1 numeric character</li><li><span>&#x2716;</span> Must have at least one special character</li></ul></div>';
+    }
+    else {
+        errorCont = (_h = password
+            .closest('.input')) === null || _h === void 0 ? void 0 : _h.querySelector('.error');
+        errorCont.innerHTML = '';
+    }
+    if (confirmPassword.value !== password.value) {
+        errorCont = (_j = confirmPassword
+            .closest('.input')) === null || _j === void 0 ? void 0 : _j.querySelector('.error');
+        errorCont.innerHTML =
+            '<div class="error-inner"><p><span>&#x2716;</span> Passwords do not match</p></div>';
+    }
+    else {
+        errorCont = (_k = confirmPassword
+            .closest('.input')) === null || _k === void 0 ? void 0 : _k.querySelector('.error');
+        errorCont.innerHTML = '';
+    }
 });
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,93 @@
-// const root: HTMLElement | null = document.getElementById('root')
-const root = document.getElementById('root') as HTMLElement
+const root = document.getElementById('root') as HTMLDivElement
+
+// Switch button (Dark/Light Mode)
 const switchBtn = root.querySelector('.switch') as HTMLButtonElement
 
 switchBtn.addEventListener('click', () => {
 	root.classList.toggle('dark')
+})
+
+// Form handling
+const form = root.querySelector('form') as HTMLFormElement
+const firstName = form.querySelector('[name="firstName"]') as HTMLInputElement
+const lastName = form.querySelector('[name="lastName"]') as HTMLInputElement
+const email = form.querySelector('[name="email"]') as HTMLInputElement
+const password = form.querySelector('[name="password"]') as HTMLInputElement
+const confirmPassword = form.querySelector(
+	'[name="confirmPassword"]'
+) as HTMLInputElement
+const submit = form.querySelector('[type="submit"]') as HTMLButtonElement
+let errorCont: HTMLDivElement
+
+submit.addEventListener('click', e => {
+	e.preventDefault()
+
+	if (firstName.value.trim() === '') {
+		errorCont = firstName
+			.closest('.input')
+			?.querySelector('.error') as HTMLDivElement
+		errorCont.innerHTML =
+			'<div class="error-inner"><p><span>&#x2716;</span> Please enter your first name</p></div>'
+	} else {
+		errorCont = firstName
+			.closest('.input')
+			?.querySelector('.error') as HTMLDivElement
+		errorCont.innerHTML = ''
+	}
+
+	if (lastName.value.trim() === '') {
+		errorCont = lastName
+			.closest('.input')
+			?.querySelector('.error') as HTMLDivElement
+		errorCont.innerHTML =
+			'<div class="error-inner"><p><span>&#x2716;</span> Please enter your last name</p></div>'
+	} else {
+		errorCont = lastName
+			.closest('.input')
+			?.querySelector('.error') as HTMLDivElement
+		errorCont.innerHTML = ''
+	}
+
+	if (!email.value.match(/\S+@\S+\.\S+/)) {
+		errorCont = email
+			.closest('.input')
+			?.querySelector('.error') as HTMLDivElement
+		errorCont.innerHTML =
+			'<div class="error-inner"><p><span>&#x2716;</span> Enter a valid email address</p></div>'
+	} else {
+		errorCont = email
+			.closest('.input')
+			?.querySelector('.error') as HTMLDivElement
+		errorCont.innerHTML = ''
+	}
+
+	if (
+		!password.value.match(
+			/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,}$/
+		)
+	) {
+		errorCont = password
+			.closest('.input')
+			?.querySelector('.error') as HTMLDivElement
+		errorCont.innerHTML =
+			'<div class="error-inner"><ul><li><span>&#x2716;</span> Must be at least 6 characters long</li><li><span>&#x2716;</span> Must have at least 1 numeric character</li><li><span>&#x2716;</span> Must have at least one special character</li></ul></div>'
+	} else {
+		errorCont = password
+			.closest('.input')
+			?.querySelector('.error') as HTMLDivElement
+		errorCont.innerHTML = ''
+	}
+
+	if (confirmPassword.value !== password.value) {
+		errorCont = confirmPassword
+			.closest('.input')
+			?.querySelector('.error') as HTMLDivElement
+		errorCont.innerHTML =
+			'<div class="error-inner"><p><span>&#x2716;</span> Passwords do not match</p></div>'
+	} else {
+		errorCont = confirmPassword
+			.closest('.input')
+			?.querySelector('.error') as HTMLDivElement
+		errorCont.innerHTML = ''
+	}
 })
